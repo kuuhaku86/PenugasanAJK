33f79079376de27258b743e2cd7c9609dc448bb3
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2019-05-25 01:17:29 +0700

    repair config.php

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
-    $hostname  = "localhost";
-    $username  = "kuuhaku86";
-    $password  = "Yhn12345678!";
-    $dbname  = "penugasanajk";
-    $db = new mysqli($hostname, $username, $password, $dbname);
-?>
+
+$db_host = "localhost";
+$db_user = "kuuhaku86";
+$db_pass = "Yhn12345678!";
+$db_name = "penugasanajk";
+
+try {    
+    //create PDO connection 
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    //show error
+    die("Terjadi masalah: " . $e->getMessage());
+}
