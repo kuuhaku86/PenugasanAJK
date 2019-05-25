2e7b13f9d2f65f9619194a43836c09a700959a10
kuuhaku86 <yohan.ardiansyah90@gmail.com>
2019-05-25 10:10:42 +0700

    work, kurang edit dan delete

--- index.php
+++ index.php
@@ -9,14 +9,11 @@
 </head>
 <body>
     <div class="container">
-        <div class="big_title">Hello, Welcome to My Site</div>
-        <div class="login_box" style="padding-top:40px; height:210px; width:330px; padding-left:30px;">
-            <div class="button">
-                <a href="login.php" style="color:white; text-decoration:none; font-size:20px;font-weight:600;">Login</a>
-            </div>
-            <div class="button">
-                <a href="register.php" style="color:white; text-decoration:none; font-size:20px;font-weight:600;">Register</a>
-            </div>
+        <div class="big_title" >Welcome to My Site</div>
+        <div class="login_box" style="padding-top:25px; height:210px; width:330px; padding-left:30px; margin-top:100px;">
+            <a href="login.php" style="color:grey; text-decoration:none; font-size:20px;font-weight:600;"><div class="button">Login</div></a>
+            <a href="register.php" style="color:grey; text-decoration:none; font-size:20px;font-weight:600;"><div class="button">Register</div></a>
+            
         </div>
     </div>
 </body>
